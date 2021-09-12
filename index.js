af9618d13dcc7f1aac3d56686ecddeb0ff9d0eaf
Maryann Nwude <44924494+MaryannN@users.noreply.github.com>
2021-09-12 14:36:05 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+/*const axios = require('axios');
+//document.getElementById("submit").addEventListener("click", async () => {
+let input = document.getElementById("insert").value;
+const form = document.querySelector(".form-data");*/
+/*if (input === "finance")
+{
+    let string = "money, different kinds of finance";
+    document.getElementById("insert").innerHTML = string;
+}
+const findterm = async input => {
+try { */
+    //let input = document.getElementById("insert").value;
+    /*const summary = await axios.get("https://en.wikipedia.org/api/rest_v1/page/summary/" + input);
+    alert(input);
+    let extract = summary.data.extract;
+    extract = extract.split(". ").filter((val, index) => index < 3).join(". ");
+    alert(extract); //wiki definition
+    return extract;
+}catch(error){
+    console.error(JSON.stringify(error));
+    alert(error); //type of wiki error
+}};
+
+const handleSubmit = async e => {
+    e.preventDefault();
+    findterm(input.value);
+    console.log(input.value);
+};
+
+forms.addEventListener("submit", e => handleSubmit(e));*/
